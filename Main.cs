a68865734519b7293b9a4525b2e4fd0ff5e90d98
Jahswant <jahswant@live.com>
2024-04-08 09:38:37 -0400

    Added Additioner et Soustraire for Ints

--- Main.cs
+++ Main.cs
@@ -1,7 +1,15 @@
 class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, GitHub! Je m'appelle JAHSWANT FODOUOP TAKOGUEM");
-        }
+      Console.WriteLine("Hello, GitHub! Je m'appelle JAHSWANT FODOUOP TAKOGUEM");
     }
+}
+
+public static int Additioner(int a, int b){
+    return a + b;
+}
+
+public static int Soustraire(int a, int b){
+    return a - b;
+}
